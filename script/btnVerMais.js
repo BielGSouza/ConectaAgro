378e711cb2125a427e19ff8e826154e47a19d312
Gabriel de Souza <gabrielsouzaca05@gmail.com>
2026-05-19 18:14:37 -0300

    Finalizado Pagina News
    Finalizei a pagina news e agora irei começar o Store

--- script/btnVerMais.js
+++ script/btnVerMais.js
@@ -25,7 +25,7 @@ window.addEventListener("scroll", () => {
 
     const fimDaPagina =
         window.innerHeight + window.scrollY
-        >= document.body.offsetHeight - 100;
+        >= document.body.offsetHeight - 500;
 
     if (fimDaPagina && contador <= 20) {
         mostrarMaisNoticias();
@@ -33,8 +33,7 @@ window.addEventListener("scroll", () => {
 
 });
 
-const novidades =
-    document.getElementById("div-novidades");
+const novidades = document.getElementById("div-novidades");
 
 novidades.addEventListener("wheel", (event) => {
 
@@ -43,3 +42,21 @@ novidades.addEventListener("wheel", (event) => {
     novidades.scrollLeft += event.deltaY;
 
 });
+
+function verMais() {
+    const divNoticias = document.getElementById("div-card-horizontal");
+    
+    for (let i = 0; i < 5; i++) {
+        const div = document.createElement("div")
+
+        div.classList.add("card-horizontal");
+
+        div.innerHTML = `
+            <img src="../assets/sectionQuemSomos/photoSecundaria.png" alt="Notícia ${contador}" height="75%">
+            <h4 class="h3-news card-title">Título da Notícia ${contador}</h4>
+        `;
+
+        divNoticias.appendChild(div);
+        contador++;
+    }
+}
